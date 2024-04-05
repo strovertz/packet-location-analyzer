e7165e742da85a5fb8f28722562c81440bfcc7d4
strovertz <gleisonaps@hotmail.com>
2024-04-04 21:50:43 -0300

    Atualizando dados e adicionando funcao para ler pcap

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import pydivert
 import requests
 import thread6
+from scapy.utils import RawPcapReader
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP
 from collections import Counter
 from get_loc import *
 from map import *
@@ -21,9 +24,10 @@ def package_load():
 #    print(lista2)
 
 #@thread6.threaded()
+# Use para ler os pacotes que da rede
 def capture_package():
     lista = []; lista2 = []; ips = []; bit = 0
-    prefix = ['172', '192', '10.',  '127', '255']
+    prefix = ['172', '192', '10.', '127', '255', 'fe8']
     with pydivert.WinDivert() as w:
         for packet in w:
             #if packet.dst_addr == "3.95.214.97": # ip instancia ec2 com um api/rest rodando em docker
@@ -31,10 +35,27 @@ def capture_package():
             print(packet)
             # para checar a porta, implementar if para validar direção e checagem jogar a porta em uma lista
             w.send(packet)
-            if bit == 100: break
+            if bit == 300: break
             bit+=1
             #else: continue
     return ips
+
+# use pra ler pacotes de um arquivo pcap
+def read_pcap(file_path):
+    pcap_ips = []
+    prefix = ['172', '192', '10.', '127', '255', 'fe8']
+    for (packet_data, packet_metadata,) in RawPcapReader(file_path):
+        packet_eth = Ether(packet_data)
+        #ignora se nao for ipv4
+        if packet_eth.type != 0x0800:
+            continue
+        packet_ip = packet_eth[IP]
+        if packet_ip.proto != 6:
+           continue
+        if packet_ip.src[:3] not in prefix and packet_ip.src not in pcap_ips: pcap_ips.append(packet_ip.src)
+    return pcap_ips
+
+
 
 def insert_locs(ips):
     address = process_ips(ips)
@@ -42,20 +63,20 @@ def insert_locs(ips):
     print(address)
     j = 0
     for i in address:
-        print(f'IP DE I AQUI: {i}')
-        infos = get_infos(ips[j])
-        if len(i) > 1: mapa = set_markup(mapa, i, ips[j], infos)
+        if len(i) > 1: infos = get_infos(ips[j]); mapa = set_markup(mapa, i, ips[j], infos); print(f'Lat,Lon de ip \'{j}\': {i}')
         j+=1
-    mapa = set_markup(mapa,[len(address)-2,len(address)-1], ips[len(ips)-1], get_infos(ips[len(ips)-1]))
+    if len(address) > 0: mapa = set_markup(mapa,[len(address)-2,len(address)-1], ips[len(ips)-1], get_infos(ips[len(ips)-1]))
     mapa.save("../map/my_map1.html")
 
     print('Mapa Salvo')
 
 def main():
+    file_path = "files/trabalho1.pcapng"
     #thread6.run_threaded(package_load)
     #thread6.run_threaded(capture_package)
-    ips = capture_package()
-    insert_locs(ips)
+    #ips = capture_package()
+    ips2 = read_pcap(file_path)
+    insert_locs(ips2)
 
 if __name__ == "__main__":
     main()
